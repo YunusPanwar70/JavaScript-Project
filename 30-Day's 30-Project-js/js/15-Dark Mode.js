910375d77b6d02800656dba1ed5c436c28edea26
YunusPanwar <yunuspanwar78@gmail.com>
2023-12-02 14:58:24 +0530

    make some project

--- 30-Day's 30-Project-js/js/15-Dark Mode.js
+++ 30-Day's 30-Project-js/js/15-Dark Mode.js
@@ -3,8 +3,8 @@ let icon = document.getElementById("icon");
 icon.onclick = function() {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
-        icon.src = "../img/sun.jpeg";
+        icon.src = "../img/sun.png";
     } else {
-        icon.src = "../img/moon.jpeg"
+        icon.src = "../img/moon.png"
     }
 }
---
+++ 30-Day's 30-Project-js/js/7-Make Quote Generator Website.js
@@ -0,0 +1,26 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author")
+const api_url = "https://api.quotable.io/random";
+
+
+async function getquote(url) {
+    const response = await fetch(url);
+    let data = await response.json();
+    quote.innerHTML = data.content;
+    author.innerHTML = data.content;
+}
+
+getquote(api_url);
+
+
+let button = document.getElementById("btn");
+button.addEventListener("click", () => {
+    getquote(api_url);
+
+})
+
+let btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", () => {
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "---- by " + author.innerHTML, "Tweet Window", "width=600,height=300")
+
+})
