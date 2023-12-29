74e8ff108fdabfd9ec4c6329a726eee815cc8c03
YunusPanwar <yunuspanwar78@gmail.com>
2023-12-29 18:42:54 +0530

    Make JS Project

---
+++ 30-Day's 30-Project-js/29-Drag & Drop/js/script.js
@@ -0,0 +1,27 @@
+let listItems = document.getElementsByClassName("list");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+
+for (let item of listItems) {
+    item.addEventListener("dragstart", function(e) {
+        let selected = e.target;
+
+        rightBox.addEventListener("dragover", function(e) {
+            e.preventDefault();
+        });
+
+        rightBox.addEventListener("drop", function(e) {
+            rightBox.appendChild(selected);
+            selected = null;
+        });
+
+        leftBox.addEventListener("dragover", function(e) {
+            e.preventDefault();
+        });
+
+        leftBox.addEventListener("drop", function(e) {
+            leftBox.appendChild(selected);
+            selected = null;
+        });
+    });
+};
